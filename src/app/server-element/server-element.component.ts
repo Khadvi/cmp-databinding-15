62717abc539f127fcca499d8dffb7b4582f938cf
khadvi.pidikiti <khaaddwi.pidikiti.09@gmail.com>
2025-05-24 17:35:54 +0530

    spliting components and Input()

---
+++ src/app/server-element/server-element.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  standalone: false,
+  templateUrl: './server-element.component.html',
+  styleUrl: './server-element.component.css'
+})
+export class ServerElementComponent {
+  @Input() element!: { type: string; name: string; content: string; };
+}
