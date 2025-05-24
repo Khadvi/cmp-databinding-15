e437e75658a3ce53cc87eb5e311f85130a7e1436
khadvi.pidikiti <khaaddwi.pidikiti.09@gmail.com>
2025-05-24 19:54:52 +0530

    access from tempalte with @ViewChild

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -9,15 +9,17 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class CockpitComponent {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output() bluePrintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  newServerName = '';
-  newServerContent = '';
+  // newServerName = '';
+  // newServerContent = '';
+
+  @ViewChild('serverContentInput') serverContentInput!: ElementRef;
 
   onAddServer(inputServerName: HTMLInputElement) {
-    this.serverCreated.emit({serverName: inputServerName.value, serverContent: this.newServerContent})
+    this.serverCreated.emit({serverName: inputServerName.value, serverContent: this.serverContentInput.nativeElement.value})
   }
 
-  onAddBlueprint() {
-    this.bluePrintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent})
+  onAddBlueprint(inputServerName: HTMLInputElement) {
+    this.bluePrintCreated.emit({serverName: inputServerName.value, serverContent: this.serverContentInput.nativeElement.value})
   }
 
 }
